3f1d051cea1ca10eb4971aaa8ec17f0fd5503dc2
Monerat <gmonerat@gmail.com>
2023-09-14 22:07:00 -0300

    tirando os executavel

--- ProjetoPooGrupo3/principal/Principal.java
+++ ProjetoPooGrupo3/principal/Principal.java
@@ -23,7 +23,6 @@ public class Principal {
 	
 	public static void main(String[] args) {
 		
-		//Criar o database
 		if (configInicial()) {
 			if (CreateDAO.createBD(BANCO, SCHEMA, dadosCon)) {
 				con = new Conexao(dadosCon); 
--- executavel.bat
+++
@@ -1,3 +0,0 @@
-chcp 65001
-"D:\Programação\SerraTEC\Programação Orientada a Objeto\jdk-17.0.8\bin\java.exe" -jar projetoG3.jar
-pause
